0a5a75e3240365ec6dc315c73ea9c4629dace319
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2025-02-07 05:23:52 +0200

    style: Improve Navbar styling for better UI consistency - Adjusted spacing and alignment for better responsiveness.

--- src/Components/NavBarSection.js
+++ src/Components/NavBarSection.js
@@ -4,13 +4,20 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../Styles/Navbar.css"; // Import the CSS file
+import "../Styles/Navbar.css";
 
 function NavBar() {
   return (
-    <Navbar expand="lg" className="custom-navbar" variant="dark">
+    <Navbar
+      expand="lg"
+      className="custom-navbar"
+      variant="dark"
+      aria-label="Main Navigation"
+    >
       <Container>
-        <Navbar.Brand>Boghdaddy's Code</Navbar.Brand>
+        <Navbar.Brand aria-label="Boghdaddy's Code Brand">
+          Boghdaddy's Code
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
@@ -18,21 +25,23 @@ function NavBar() {
               activeClass="navbar--active-content"
               spy={true}
               smooth={true}
-              offset={10}
-              duration={100}
+              offset={-70}
+              duration={500}
               to="SkillsSection"
               className="nav-link"
+              aria-label="Navigate to Skills Section"
             >
               Skills
             </ScrollLink>
             <ScrollLink
               activeClass="navbar--active-content"
               spy={true}
-              smooth={false}
-              offset={10}
-              duration={100}
+              smooth={true}
+              offset={-70}
+              duration={500}
               to="ProjectsSection"
               className="nav-link"
+              aria-label="Navigate to Projects Section"
             >
               Projects
             </ScrollLink>
@@ -40,10 +49,11 @@ function NavBar() {
               activeClass="navbar--active-content"
               spy={true}
               smooth={true}
-              offset={10}
-              duration={100}
+              offset={-70}
+              duration={500}
               to="contact"
               className="nav-link"
+              aria-label="Navigate to Contact Section"
             >
               Contact
             </ScrollLink>
@@ -51,12 +61,13 @@ function NavBar() {
               activeClass="navbar--active-content"
               spy={true}
               smooth={true}
-              offset={10}
-              duration={100}
+              offset={-70}
+              duration={500}
               to="WorkExperienceSection"
               className="nav-link"
+              aria-label="Navigate to Work Experience Section"
             >
-              WorkExperience
+              Work Experience
             </ScrollLink>
           </Nav>
         </Navbar.Collapse>
